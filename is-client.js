1df7472c5f822416d036e65e106bb147e248f0ed
Bjorn Tipling <bjorn@ambientchill.com>
2015-10-24 18:45:34 -0700

    Clean up repetitive code.

--- is-client.js
+++ is-client.js
@@ -1,17 +1,2 @@
-module.exports = function (Babel) {
-  const Plugin = Babel.Plugin;
-  const types = Babel.types;
-  return new Plugin("iso-babel", {
-    visitor: {
-      IfStatement: function (node, parent) {
-        if (node.test.name === 'isServer') {
-          this.dangerouslyRemove();
-        }
-        if (node.test.name === 'isClient') {
-          return node.consequent.body;
-        }
-      }
-    }
-  });
-}
-
+const pluginGenerator = require('./lib/plugin-generator');
+module.exports = pluginGenerator('isServer', 'isClient');
--- is-server.js
+++ is-server.js
@@ -1,17 +1,2 @@
-module.exports = function (Babel) {
-  const Plugin = Babel.Plugin;
-  const types = Babel.types;
-  return new Plugin("iso-babel", {
-    visitor: {
-      IfStatement: function (node, parent) {
-        if (node.test.name === 'isClient') {
-          this.dangerouslyRemove();
-        }
-        if (node.test.name === 'isServer') {
-          return node.consequent.body;
-        }
-      }
-    }
-  });
-}
-
+const pluginGenerator = require('./lib/plugin-generator');
+module.exports = pluginGenerator('isClient', 'isServer');
